64194ca2d14da6c807b0d651da181fefd0d95311
Kaiqiang Dawn <moonshile@foxmail.com>
2015-06-13 14:57:27 +0800

    update

--- README.md
+++ README.md
@@ -3,16 +3,25 @@ Chinese word segmentation algorithm without corpus
 
 ## Usage
 ```
-from wordseg import wordSegment
+from wordseg import WordSegment
 doc = u'十四是十四四十是四十，十四不是四十，四十不是十四'
-wordSegment(doc, max_word_len=2, min_aggregation=1, min_entropy=0.5)
+ws = WordSegment(doc, max_word_len=2, min_aggregation=1, min_entropy=0.5)
+ws.segSentence(doc)
 ```
 
-This will generate words `十四`, `四十`, `不是`
+This will generate words
+
+`十四 是 十四 四十 是 四十 ， 十四 不是 四十 ， 四十 不是 十四`
 
 In fact, `doc` should be a long enough document string for better results. In that condition, the min_aggregation should be set far greater than 1, such as 50, and min_entropy should also be set greater than 0.5, such as 1.5.
 
 Besides, both input and output of this function should be decoded as unicode.
+
+`WordSegment.segSentence` has an optional argument `method`, with values `WordSegment.L`, `WordSegment.S` and `WordSegment.ALL`, means
+
++ `WordSegment.L`: if a long word that is combinations of several shorter words found, given only the long word.
++ `WordSegment.S`: given the several shorter words.
++ `WordSegment.ALL`: given both the long and the shorters.
 
 ## Reference
 
--- wordseg.py
+++ wordseg.py
@@ -9,7 +9,7 @@ Reference: http://www.matrix67.com/blog/archives/5044
 import re
 
 from probability import entropyOfList
-from sequence import genSubparts
+from sequence import genSubparts, genSubstr
 
 
 
@@ -55,35 +55,77 @@ class WordInfo(object):
 
 
 
-def wordSegment(doc, max_word_len=5, min_freq=0.00005, min_entropy=2.0, min_aggregation=50):
-    pattern = re.compile(u'[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“‘｛｝【】（）…￥！—┄－]+')
-    doc = re.sub(pattern, '', doc)
-    suffix_indexes = indexOfSortedSuffix(doc, max_word_len)
-    word_cands = {}
-    # compute frequency and neighbors
-    for suf in suffix_indexes:
-        word = doc[suf[0]:suf[1]]
-        if word not in word_cands:
-            word_cands[word] = WordInfo(word)
-        word_cands[word].update(doc[suf[0] - 1:suf[0]], doc[suf[1]:suf[1] + 1])
-    # compute probability and entropy
-    length = len(doc)
-    for k in word_cands:
-        word_cands[k].compute(length)
-    # compute aggregation of words whose length > 1
-    values = sorted(word_cands.values(), key=lambda x: len(x.text))
-    for v in values:
-        if len(v.text) == 1: continue
-        v.computeAggregation(word_cands)
-    # words which satisfies the conditions
-    sat_words = {}
-    for v in values:
-        if len(v.text) > 1 and v.aggregation > min_aggregation and\
-                v.freq > min_freq and v.left > min_entropy and v.right > min_entropy:
-            sat_words[v.text] = v
-    return map(lambda x: x.text, sorted(sat_words.values(), key=lambda v: v.freq, reverse=True))
+class WordSegment(object):
 
+    """
+    Main class for Chinese word segmentation
+    1. Generate words from a long enough document
+    2. Do the segmentation work with the document
+    """
 
+    # if a word is combination of other shorter words, then treat it as a long word
+    L = 0
+    # if a word is combination of other shorter words, then treat it as the set of shortest words
+    S = 1
+    # if a word contains other shorter words, then return all possible results
+    ALL = 2
+
+    def __init__(self, doc, max_word_len=5, min_freq=0.00005, min_entropy=2.0, min_aggregation=50):
+        super(WordSegment, self).__init__()
+        self.max_word_len = max_word_len
+        self.min_freq = min_freq
+        self.min_entropy = min_entropy
+        self.min_aggregation = min_aggregation
+        self.words = self.genWords(doc)
+
+    def genWords(self, doc):
+        pattern = re.compile(u'[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“‘｛｝【】（）…￥！—┄－]+')
+        doc = re.sub(pattern, '', doc)
+        suffix_indexes = indexOfSortedSuffix(doc, self.max_word_len)
+        word_cands = {}
+        # compute frequency and neighbors
+        for suf in suffix_indexes:
+            word = doc[suf[0]:suf[1]]
+            if word not in word_cands:
+                word_cands[word] = WordInfo(word)
+            word_cands[word].update(doc[suf[0] - 1:suf[0]], doc[suf[1]:suf[1] + 1])
+        # compute probability and entropy
+        length = len(doc)
+        for k in word_cands:
+            word_cands[k].compute(length)
+        # compute aggregation of words whose length > 1
+        values = sorted(word_cands.values(), key=lambda x: len(x.text))
+        for v in values:
+            if len(v.text) == 1: continue
+            v.computeAggregation(word_cands)
+        # words which satisfies the conditions
+        sat_words = {}
+        for v in values:
+            if len(v.text) > 1 and v.aggregation > self.min_aggregation and\
+                    v.freq > self.min_freq and v.left > self.min_entropy and v.right > self.min_entropy:
+                sat_words[v.text] = v
+        return set(map(lambda x: x.text, sorted(sat_words.values(), key=lambda v: v.freq, reverse=True)))
+
+    def segSentence(self, sentence, method=ALL):
+        i = 0
+        res = []
+        while i < len(sentence):
+            if method == self.L or method == self.S:
+                j_range = range(self.max_word_len, 0, -1) if method == self.L else range(2, self.max_word_len + 1) + [1]
+                for j in j_range:
+                    if j == 1 or sentence[i:i + j] in self.words:
+                        res.append(sentence[i:i + j])
+                        i += j
+                        break
+            else:
+                to_inc = 1
+                for j in range(2, self.max_word_len + 1):
+                    if i + j <= len(sentence) and sentence[i:i + j] in self.words:
+                        res.append(sentence[i:i + j])
+                        if to_inc == 1: to_inc = j
+                if to_inc == 1: res.append(sentence[i])
+                i += to_inc
+        return res
 
 
 
